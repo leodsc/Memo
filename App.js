7fc019fdef8567cc5d652930c4b02778f9ffa1aa
Carol <carolineferreirap@gmail.com>
2021-05-03 10:54:46 -0300

    Adicionando tela inicial logo

--- App.js
+++ App.js
@@ -1,33 +1,25 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { 
-  AlarmeMsg, WhatsMsg, 
-  AlertMsg, DeleteMsg } 
-  from './warns/msgTypes';
-import { getContainer } from './warns/buttons';
-import ContactsWindow from './whats/addContact';
-import { Index } from './components/index';
-
-// use this function to create a message
-const createMsg = (msgType) => {
-  // argument is a component that you want to create
-  const msg = <View>{React.createElement(msgType)}</View>;
-  getContainer(msg);
-  return msg;
-}
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 class App extends Component {
   render() {
     // const alertMsg = createMsg(AlertMsg);
     return (
-      <Index
-      textStyle={
-        {fontSize: 20, position: 'relative', fontFamily: 'Roboto',
-        color: 'white', top: '25%', textAlign: 'center'}}
-      text={"Meus Remédios"}
-      bannerState={{uri: 'https://i.ibb.co/7CPTDR0/add.png'}}/>
+        <View style={styles.container}>
+            <Image source={require('./images/Group.png')} />
+            <Image source={require('./images/memory.png')} />
+        </View>
     )
   }
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#12263A',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+});
